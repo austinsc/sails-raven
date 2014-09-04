adf968ebb66e8efec7f89d1a563ab1c2cf3a03fe
Stephen Austin <saustin@arhaus.com>
2014-09-04 15:44:57 -0400

    misc cleanup

--- test/unit/register.js
+++ test/unit/register.js
@@ -2,17 +2,33 @@
  * Test dependencies
  */
 var Adapter = require('../../SailsRaven.js');
+var query = require('../../lib/query.js');
 
-describe('registerCollection', function () {
+describe('registerConnection', function () {
 
-	it('should not hang or encounter any errors', function (done) {
-		Adapter.registerCollection({
-			identity: 'foo'
-		}, done);
-	});
+  // it('should not hang or encounter any errors', function (done) {
+  //   Adapter.registerConnection({
+  //     identity: 'foo'
+  //   }, done);
+  // });
 
-	// e.g.
-	// it('should create a mysql connection pool', function () {})
-	// it('should create an HTTP connection pool', function () {})
-	// ... and so on.
+  // e.g.
+  // it('should create a mysql connection pool', function () {})
+  // it('should create an HTTP connection pool', function () {})
+  // ... and so on.
+});
+
+describe('query', function () {
+  it('should create a valid lucene query string', function (done) {
+    var q = query()
+      .from("Products")
+      .index("Type", "Chair")
+      .where("Model").equals("Accord")
+      .orderBy("PartNumber")
+      .success(function () {
+        done();
+      }).error(function (err) {
+        assert.fail(err, null, err);
+      }).select();
+  });
 });
